06ae7d8a3215e0b64089ae0ca844cc7d21ac11ce
Dmitry N. <ndk@esperanca.biz>
2025-05-02 14:41:23 -0500

    WIP: barebones

--- src/data.rs
+++
@@ -1,9 +0,0 @@
-use toml::value::Table;
-use std::fs;
-
-// Function to load and parse the credential data from the TOML file
-pub fn load_credential_data(path: &str) -> Result<Table, Box<dyn std::error::Error>> {
-    let contents = fs::read_to_string(path)?;
-    let data: Table = toml::from_str(&contents)?;
-    Ok(data)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-use gtk::prelude::*;
-use gtk::Application;
-use crate::ui::build_credential_list_ui; // Import the main UI building function
+use gtk::prelude::{ApplicationExt, ApplicationExtManual, BuilderExtManual, GtkWindowExt, WidgetExt};
+use gtk::{Application, ApplicationWindow, Builder, TreeView};
 
-mod ui; // Declare the ui module
-mod data; // Declare the data module
-
-const APP_ID: &str = "com.example.Rustillum";
+const APP_ID: &str = "org.rustillum.ui";
 
 fn main() {
     // Create a new application
-    let app = Application::builder().application_id(APP_ID).build();
+    let app = Application::builder()
+        .application_id(APP_ID)
+        .build();
 
-    // Connect to "activate" signal of `app`
-    app.connect_activate(build_credential_list_ui);
+    // Connect to "activate" signal of application
+    app.connect_activate(build_ui);
 
     // Run the application
     app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Load the UI from the glade file
+    let builder = Builder::from_file("ui/rustillum.glade");
+
+    // Get the window from the builder
+    let window: ApplicationWindow = builder.object("window").expect("Couldn't get window");
+    window.set_application(Some(app));
+
+    // Get the TreeView from the builder
+    let tree_view: TreeView = builder.object("tree_view").expect("Couldn't get tree_view");
+
+    // Present window
+    window.show_all();
 }
